3d7bd618d36fec5e5fb6a794e249c0d1770d6185
Zhou <zhou_y@corp.medplus.com>
2020-12-25 14:02:16 -0500

    setup

---
+++ server/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        '@typescript-eslint/no-var-requires': 0
+    }
+};
---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+
+const express=require('express')
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const morgan=require("morgan")
+
+const app=express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: "hello!"        
+    })
+})
+
+console.log("hello!");
+
+app.listen(process.env.PORT||8081)
+
